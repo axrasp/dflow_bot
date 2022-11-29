2de1dace43712b6388aaf7f99c2657e91992b7fc
axrasp <aaraspopin@gmail.com>
2022-11-29 18:10:15 +0300

    notification added

--- bot.py
+++ bot.py
@@ -2,17 +2,24 @@ import logging
 
 from environs import Env
 from google.cloud import dialogflow
-from telegram import Update
+from telegram import Update, Bot
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
                           MessageHandler, Updater)
 
-# Enable logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
-)
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('Logger')
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def detect_intent_texts(project_id, session_id, texts, language_code):
@@ -35,7 +42,6 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
         )
         return response.query_result.fulfillment_text
     
-
 
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
@@ -63,31 +69,38 @@ def echo(update: Update, context: CallbackContext) -> None:
 
 
 def main() -> None:
-    """Start the bot."""
-    # Create the Updater and pass it your bot"s token.
     env = Env()
     env.read_env()
     bot_token = env.str("TOKEN")
+    chat_id = env.str("CHAT_ID")
     updater = Updater(bot_token)
+    bot = Bot(bot_token)
+    logger.setLevel(logging.WARNING)
+    logger.addHandler(TelegramLogsHandler(tg_bot=bot, chat_id=chat_id))
+    while True:
+        try:
+            # Get the dispatcher to register handlers
+            dispatcher = updater.dispatcher
 
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
+            # on different commands - answer in Telegram
+            dispatcher.add_handler(CommandHandler("start", start))
+            dispatcher.add_handler(CommandHandler("help", help_command))
 
-    # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
+            # on non command i.e message - echo the message on Telegram
+            dispatcher.add_handler(MessageHandler(
+                Filters.text & ~Filters.command, echo))
+            # Start the Bot
+            updater.start_polling()
+            # Run the bot until you press Ctrl-C or the process receives SIGINT,
+            # SIGTERM or SIGABRT. This should be used most of the time, since
+            # start_polling() is non-blocking and will stop the bot gracefully.
+            updater.idle()
 
-    # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(
-        Filters.text & ~Filters.command, echo))
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+        except Exception as e:
+            logger.error('Возникла ошибка:')
+            logger.error(e, exc_info=True)
+            logger.warning('Перезапускаю бот')
+            continue
 
 
 if __name__ == "__main__":
--- vk_bot.py
+++ vk_bot.py
@@ -1,9 +1,15 @@
+import logging
 import random
 
 import vk_api as vk
 from environs import Env
 from google.cloud import dialogflow
+from telegram import Bot
 from vk_api.longpoll import VkEventType, VkLongPoll
+
+from bot import TelegramLogsHandler
+
+logger = logging.getLogger('Logger')
 
 
 def get_df_reply(project_id, session_id, texts, language_code):
@@ -39,20 +45,33 @@ def main():
     env.read_env()
     project_id = env.str("PROJECT_ID")
     vk_token = env.str("VK_TOKEN")
+    bot_token = env.str("TOKEN")
+    chat_id = env.str("CHAT_ID")
 
-    vk_session = vk.VkApi(token=vk_token)
-    vk_api = vk_session.get_api()
-    longpoll = VkLongPoll(vk_session)
+    bot = Bot(bot_token)
+    logger.setLevel(logging.WARNING)
+    logger.addHandler(TelegramLogsHandler(tg_bot=bot, chat_id=chat_id))
 
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            reply_text = get_df_reply(
-                project_id=project_id,
-                session_id=event.user_id,
-                texts=[event.text],
-                language_code="ru-RU")
-            if reply_text:
-                echo(event, vk_api, reply_text)
+    while True:
+        try:
+            vk_session = vk.VkApi(token=vk_token)
+            vk_api = vk_session.get_api()
+            longpoll = VkLongPoll(vk_session)
+
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                    reply_text = get_df_reply(
+                        project_id=project_id,
+                        session_id=event.user_id,
+                        texts=[event.text],
+                        language_code="ru-RU")
+                    if reply_text:
+                        echo(event, vk_api, reply_text)
+        except Exception as e:
+            logger.error('Возникла ошибка:')
+            logger.error(e, exc_info=True)
+            logger.warning('Перезапускаю бот')
+            continue
 
 
 if __name__ == "__main__":
