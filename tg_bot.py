691f0898646fcf4ed632e931f6ce3513c9dfbc13
axrasp <aaraspopin@gmail.com>
2022-12-05 17:54:08 +0300

    detect intens removed, bot_data added

--- tg_bot.py
+++ tg_bot.py
@@ -1,54 +1,28 @@
 import logging
 
 from environs import Env
-from google.cloud import dialogflow
 from telegram import Bot, Update
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
                           MessageHandler, Updater)
 
-from dflow_scripts import TelegramLogsHandler
+from dflow_scripts import TelegramLogsHandler, get_df_reply
 
 logger = logging.getLogger('Logger')
-
-
-def detect_intent_texts(project_id, session_id, texts, language_code):
-
-    session_client = dialogflow.SessionsClient()
-
-    session = session_client.session_path(project_id, session_id)
-    print("Session path: {}\n".format(session))
-
-    for text in texts:
-        text_input = dialogflow.TextInput(
-            text=text,
-            language_code=language_code
-        )
-
-        query_input = dialogflow.QueryInput(text=text_input)
-
-        response = session_client.detect_intent(
-            request={"session": session, "query_input": query_input}
-        )
-        return response.query_result.fulfillment_text
 
 
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
-    env = Env()
-    env.read_env()
-    context.user_data["project_id"] = env.str("PROJECT_ID")
-    context.user_data["session_id"] = update.effective_user.id
     update.message.reply_markdown_v2(
         fr"Hi {user.mention_markdown_v2()}\! Давай выкладывай",
     )
 
 
 def send_df_reply(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(detect_intent_texts(
-        project_id=context.user_data["project_id"],
-        session_id=context.user_data["session_id"],
-        texts=[update.message.text], language_code="ru-RU"))
+    update.message.reply_text(get_df_reply(
+        session_id=update.effective_user.id,
+        project_id=context.bot_data["project_id"],
+        text=update.message.text, language_code="ru-RU"))
 
 
 def main() -> None:
@@ -56,6 +30,7 @@ def main() -> None:
     env.read_env()
     bot_token = env.str("TG_TOKEN")
     tg_chat_id = env.str("TG_CHAT_ID")
+    project_id = env.str("PROJECT_ID")
     updater = Updater(bot_token)
     bot = Bot(bot_token)
     logger.setLevel(logging.WARNING)
@@ -63,9 +38,9 @@ def main() -> None:
     while True:
         try:
             dispatcher = updater.dispatcher
+            dispatcher.bot_data["project_id"] = project_id
 
             dispatcher.add_handler(CommandHandler("start", start))
-
             dispatcher.add_handler(MessageHandler(
                 Filters.text & ~Filters.command, send_df_reply))
             updater.start_polling()
